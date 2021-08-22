83da2dc66be2c9f39be8321e8b1eda2f680a3a95
Indra Yogi <indrayp18@gmail.com>
2021-08-22 22:00:26 +0800

    fix issue header and footer

--- src/containers/TheFooter.vue
+++ src/containers/TheFooter.vue
@@ -1,12 +1,12 @@
 <template>
   <CFooter :fixed="false">
     <div>
-      <a href="https://coreui.io" target="_blank">CoreUI</a>
-      <span class="ml-1">&copy; {{new Date().getFullYear()}} creativeLabs.</span>
+      <a href="https://coreui.io" target="_blank">Smart RT</a>
+      <span class="ml-1">&copy; {{new Date().getFullYear()}}</span>
     </div>
     <div class="mfs-auto">
-      <span class="mr-1" target="_blank">Powered by</span>
-      <a href="https://coreui.io/vue">CoreUI for Vue</a>
+      <span class="mr-1" target="_blank">Aplikasi Perkawinan dan Perceraian</span>
+      <a href="https://coreui.io/vue"></a>
     </div>
   </CFooter>
 </template>
